78b2a36b7c0151fc956c0cbec9a8f128e966ee59
nidhinm <nidhinmohannidhin@gmail.com>
2025-02-28 13:06:25 +0530

    updated redux with persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import tasksReducer from "../features/tasks/taskSlice";
 
+const persistConfig = {
+    key: "root",
+    storage,
+}
+
+const persistReducers = persistReducer(persistConfig, tasksReducer);
+
 export const store = configureStore({
-    reducer: {
-     tasks: tasksReducer,
-    },
-    });
+  reducer: {
+    tasks: persistReducers,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Disable warnings for redux-persist
+    }),
+});
+
+export const persistor = persistStore(store);
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-<<<<<<< HEAD
-import { store } from './redux/store.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store, persistor } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>,
-=======
-import { store } from './app/store.js'
-
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <StrictMode>  
-      <App /> 
-    </StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
->>>>>>> d8f1981 (task with redux)
 )
